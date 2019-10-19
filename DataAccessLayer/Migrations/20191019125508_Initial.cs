f90a9a11f28a3d1b483bb0e6f46f9424165543a0
Wahaha <Wahaha@DESKTOP-DGCG1BT>
2019-10-19 15:56:50 +0300

    Added migrations.

---
+++ DataAccessLayer/Migrations/20191019125508_Initial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+	public partial class Initial : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{ }
+
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{ }
+	}
+}
